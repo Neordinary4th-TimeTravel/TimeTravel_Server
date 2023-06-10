18eb20adeadcc42042d140df10d4911756efc9bc
길지운 <wldns2577@khu.ac.kr>
2023-06-10 18:05:49 +0900

    [DEL] unnecessary class OAuthService and import

--- neordinary-template-springboot-java-server/src/main/java/com/example/demo/src/user/UserController.java
+++ neordinary-template-springboot-java-server/src/main/java/com/example/demo/src/user/UserController.java
@@ -1,22 +1,10 @@
 package com.example.demo.src.user;
 
 
-import com.example.demo.common.Constant.SocialLoginType;
-import com.example.demo.common.oauth.OAuthService;
 import com.example.demo.utils.JwtService;
 import lombok.RequiredArgsConstructor;
-import com.example.demo.common.exceptions.BaseException;
-import com.example.demo.common.response.BaseResponse;
-import com.example.demo.src.user.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.List;
-
-
-import static com.example.demo.common.response.BaseResponseStatus.*;
-import static com.example.demo.utils.ValidationRegex.isRegexEmail;
 
 @Slf4j
 @RequiredArgsConstructor
